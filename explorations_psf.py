78ec61bae8c60a388797ae3bc5433c66d95b9391
tomfelker <tomfelker@gmail.com>
2019-10-08 08:50:11 -0700

    psf stuff, might be correct psf size, need to verify

--- explorations_psf.py
+++ explorations_psf.py
@@ -4,10 +4,12 @@ import os
 import glob
 import datetime
 import math
+import stn
 from tensorez.model import *
 from tensorez.util import *
 
 tf.compat.v1.enable_eager_execution()
+tf.set_random_seed(0)
 
 output_dir = os.path.join('output', 'explorations', 'psf')
 
@@ -19,30 +21,50 @@ write_image(gaussian_psf(64), os.path.join(output_dir, "psf_guess.png"), normali
 
 # all the below is cool, but probably not useful
 
+density_fudge = .0000003
 
 
 # pretend telescope stats
 aperture_diameter_m = 280e-3
-obstruction_diameter_m = 95e-3
+obstruction_diameter_m = aperture_diameter_m * 95e-3 / 280e-3
 focal_length_m = 2800e-3
-barlow_factor = 1
-pixel_size_m = 4.3e-6
-wavelengths_m = [700e-9, 530e-9, 470e-9]
+barlow_factor = 2
+sensor_pixel_size_m = 4.3e-6
+wavelengths_m = tf.convert_to_tensor([700e-9, 530e-9, 470e-9])
 
 
 effective_focal_length_m = focal_length_m * barlow_factor
 
 
+# the size of the PSF, for FFT purposes
+psf_px_full = 1024
 
-aperture_function_size = 6400
+# the size that we output / use...
+psf_px_cropped = 64
 
-#hmm, I bet there's a principled way to figure this...
-zoom_factor = 100
+
+sensor_pixel_angle_rad = sensor_pixel_size_m / effective_focal_length_m
+print(f"Pixels are {sensor_pixel_angle_rad * 180 / math.pi * 60 * 60} SoA")
+
+aperture_function_spatial_frequencies = psf_px_full * sensor_pixel_angle_rad / wavelengths_m
+
+# when doing the FFT, the aperture should appear to be this many pixels large, so that the result will have the given sensor_pixel_size (i think)
+aperture_diameter_px = aperture_function_spatial_frequencies * aperture_diameter_m
+
+print(f"aperture diameter in px would be:  {aperture_diameter_px}")
+
+
+# the aperture function, zoomed in such that the aperture fully fills the tensor
+aperture_function_size_cropped = 1024
+
+
+
+
 
 
 #Kolmogorov something something
 # no_noise_below r_0 something something
-def turbulence_2d(size, no_noise_at_scale = 3):
+def turbulence_2d(size, no_noise_at_scale = 1):
     turbulence = tf.random.normal(shape = (1, 1, 1, 1))
     steps = math.ceil(math.log(size, 2))
     for step in range(0, steps):
@@ -51,27 +73,9 @@ def turbulence_2d(size, no_noise_at_scale = 3):
         turbulence = tf.image.resize(turbulence, size = (turbulence.shape[-2] * 2, turbulence.shape[-3] * 2), method = tf.image.ResizeMethod.BICUBIC)
         if steps - step - 1 >= no_noise_at_scale:
             turbulence += tf.random.normal(shape = turbulence.shape)
-    turbulence = tf.squeeze(turbulence, axis = -4)
-    turbulence = tf.squeeze(turbulence, axis = -1)
     return turbulence[0:size, 0:size]
         
 write_image(turbulence_2d(512), os.path.join(output_dir, "turbulence_test.png"), normalize = True)
-
-
-def complex_to_unit_norm(complex_tensor, tolerance = 1e-9):
-    divisor = tf.abs(complex_tensor)
-    divisor = tf.maximum(divisor, tolerance)
-    return complex_tensor / tf.dtypes.complex(divisor, divisor)
-
-
-# todo: compute smaller wavefront to match aperture size, pad appropriately
-#wavefront_size = math.ceil(aperture_function_size / zoom_factor)
-wavefront_size = aperture_function_size
-
-# todo, don't think this is right - shouldn't it be more like, turbulence chooses temperature chooses optical path length, over wavelength mod tau gives phase?
-# but it's hand-wavingly right?  my arms are tired.
-wavefront = tf.dtypes.complex(turbulence_2d(wavefront_size), turbulence_2d(wavefront_size))
-wavefront = complex_to_unit_norm(wavefront)
 
 def complex_to_image(complex_tensor):
     return tf.stack([tf.math.real(complex_tensor) + .5, tf.math.imag(complex_tensor) + .5, tf.zeros(complex_tensor.shape)], axis = -1)
@@ -79,9 +83,9 @@ def complex_to_image(complex_tensor):
 #write_image(complex_to_image(wavefront), os.path.join(output_dir, "wavefront.png"))
 
 aperture_softness_px = 1
-aperture_softness_m = aperture_diameter_m / aperture_function_size * aperture_softness_px * zoom_factor
+aperture_softness_m = aperture_diameter_m / aperture_function_size_cropped * aperture_softness_px
 
-aperture_function_coord = tf.linspace(-zoom_factor * aperture_diameter_m / 2.0, zoom_factor * aperture_diameter_m / 2.0, num = aperture_function_size)
+aperture_function_coord = tf.linspace(-aperture_diameter_m / 2.0, aperture_diameter_m / 2.0, num = aperture_function_size_cropped)
 aperture_function_x = tf.expand_dims(aperture_function_coord, axis = -2)
 aperture_function_y = tf.expand_dims(aperture_function_coord, axis = -1)
 
@@ -92,28 +96,61 @@ aperture_function_outer_edge = ((aperture_diameter_m / 2.0) - aperture_function_
 aperture_function_central_obstruction = (aperture_function_r - (obstruction_diameter_m / 2.0)) / aperture_softness_m
 
 aperture_function = tf.minimum(aperture_function_outer_edge, aperture_function_central_obstruction)
-#aperture_function = aperture_function_outer_edge
 
 aperture_function = tf.clip_by_value(aperture_function, 0.0, 1.0)
 
+aperture_function = tf.expand_dims(aperture_function, axis = -1)
+aperture_function = tf.expand_dims(aperture_function, axis = -4)
+
+
 write_image(aperture_function, os.path.join(output_dir, 'aperture_function.png'))
 
-aperture_function_complex = tf.dtypes.complex(aperture_function, tf.zeros_like(aperture_function))
+# some noise with a certain power spectrum, Kolmogorov magic
+turbulence = turbulence_2d(aperture_function_size_cropped)
 
-aperture_function_complex *= wavefront
 
-write_image(complex_to_image(aperture_function_complex), os.path.join(output_dir, "aperture_function_complex.png"))
+psfs = []
+for channel in range(0, wavelengths_m.shape[0]):
 
-psf = tf.signal.ifft2d(aperture_function_complex)
+    
+    
+    # some more magic, to turn it into the difference in optical path length
+    turbulence_opd = turbulence * density_fudge
 
-#seems I don't have this yet...
-#psf = tf.signal.fftshift(psf)
-# so same thing:
-psf = tf.roll(psf, shift = (psf.shape[-2] // 2, psf.shape[-1] // 2), axis = (-2, -1))
+    # now as a phase delay, probably ignoring a 2pi factor that could be baked into fudge
+    turbulence_phase = turbulence_opd / wavelengths_m[channel]
+   
 
-# seems like i'm doing a pointless sqrt, but x * conj(x) would be doing extra stuff to compute the zero complex term - wonder if there's a better way...
-psf = tf.math.square(tf.math.abs(psf))
+    inv_scale = psf_px_full / aperture_diameter_px[channel]
+    aperture_function_scaled = stn.spatial_transformer_network(aperture_function, [[ inv_scale, 0, 0, 0, inv_scale, 0]], [psf_px_full, psf_px_full])
+    turbulence_scaled = stn.spatial_transformer_network(turbulence_phase, [[inv_scale, 0, 0, 0, inv_scale, 0]], [psf_px_full, psf_px_full])
 
-psf /= tf.reduce_max(psf)
+    wavefront = tf.exp(tf.dtypes.complex(tf.zeros_like(turbulence_scaled), turbulence_scaled))
+    
+    aperture_function_complex = tf.dtypes.complex(aperture_function_scaled, tf.zeros_like(aperture_function_scaled))
+    aperture_function_complex *= wavefront
 
-write_image(psf, os.path.join(output_dir, 'psf.png'))
+    #fft doesn't like extra dims
+    aperture_function_complex = tf.squeeze(aperture_function_complex, axis = -4)
+    aperture_function_complex = tf.squeeze(aperture_function_complex, axis = -1)
+    
+    write_image(complex_to_image(aperture_function_complex), os.path.join(output_dir, f"aperture_function_complex_ch{channel}.png"))
+
+    psf = tf.signal.ifft2d(aperture_function_complex)
+
+    #seems I don't have this yet...
+    #psf = tf.signal.fftshift(psf)
+    # so same thing:
+    psf = tf.roll(psf, shift = (psf.shape[-2] // 2, psf.shape[-1] // 2), axis = (-2, -1))
+    
+    # seems like i'm doing a pointless sqrt, but x * conj(x) would be doing extra stuff to compute the zero complex term - wonder if there's a better way...
+    psf = tf.math.square(tf.math.abs(psf))
+    
+    psfs.append(psf)
+psfs = tf.stack(psfs, axis = -1)
+
+
+
+psfs /= tf.reduce_max(psfs)
+
+write_image(psfs, os.path.join(output_dir, 'psf.png'))
