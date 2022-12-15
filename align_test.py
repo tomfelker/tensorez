51957a6d96e136660ef551751354ff38276b4fe6
tomfelker <tomfelker@gmail.com>
2022-12-14 21:15:15 -0800

    align_test works now
    I guess main change is to only align to one image, and to align to one image at a time, for faster tuning at least

--- align_test.py
+++ align_test.py
@@ -13,44 +13,35 @@ import glob
 
 output_path = os.path.join('output', 'align_test')
 
-lights = ImageSequence(os.path.join('data', '2022-01-11_jwst', '2022-01-11-0941_0-CapObj.SER'), frame_step = 10)
-darks = ImageSequence(os.path.join('data', '2022-01-11_jwst', '2022-01-11-1027_1-CapObj.SER'), frame_step = 10)
+lights = ImageSequence(os.path.join('data', '2022-01-11_jwst', '2022-01-11-0941_0-CapObj.SER'), frame_step = 1)
+darks = ImageSequence(os.path.join('data', '2022-01-11_jwst', '2022-01-11-1027_1-CapObj.SER'), frame_step = 1)
 
 #file_glob = os.path.join('data', '2022-12-07_moon_mars_conjunction', 'moon_prime', 'lights', '*.SER'); frame_step = 10
 #file_glob_darks = None # os.path.join('data', '2022-12-07_moon_mars_conjunction', 'moon_prime', 'darks', '*.SER');
 
-learning_rate_translation = 1e-6
+learning_rate_translation = 1e-3
 learning_rate_rotation = learning_rate_translation
 max_update_translation_pixels = 0.9
-max_update_rotation_pixels = 0.9
-max_steps = 30
-passes_per_lod = 10
-skip_lods = 3
+max_update_rotation_pixels = 3 #0.9
+max_steps = 50
+skip_lods = 0
 lod_factor = 1.3
 min_size = 32
-blur_pixels = 8+1
-weightedness = 50
-participation_prize = .01
+blur_pixels = 16
+incrementally_improve_target_image = False
 
-def read_average_image_with_alignment_transforms(lights, alignment_transforms, losses, image_shape, dark_image):
-    weights = tf.nn.softmax(weightedness * -losses)
-    weights = weights + participation_prize
-    weights = weights / tf.reduce_sum(weights)
+def read_average_image_with_alignment_transforms(lights, alignment_transforms, image_shape, dark_image):
 
-    #print(f'weights: {weights}')
-
-    target_image = tf.zeros(image_shape)
+    target_image = tf.Variable(tf.zeros(image_shape))
     for image_index, image_bhwc in enumerate(lights):    
-
-        # must be in the loop
         if dark_image is not None:
             image_bhwc = image_bhwc - dark_image
 
         alignment_transform = alignment_transforms[image_index, :]
         image_bhwc = align.transform_image(image_bhwc, alignment_transform)
-        target_image = target_image + image_bhwc * weights[image_index]
+        target_image.assign_add(image_bhwc)
 
-    return target_image
+    return target_image / len(lights)
 
 @tf.function
 def with_zero_mean_and_unit_variance(image_bhwc):
@@ -60,34 +51,37 @@ def with_zero_mean_and_unit_variance(image_bhwc):
 
 
 @tf.function
-def align_image_training_step(image_bhwc, alignment_transform, target_image, mask_image, learning_rate_for_lod, max_update_for_lod):
-    variable_list = [alignment_transform]
+def align_image_training_loop(image_bhwc, alignment_transform, target_image, mask_image, learning_rate_for_lod, max_update_for_lod, max_steps):
 
-    with tf.GradientTape() as tape:
-        tape.watch(variable_list)
-        alignment_guess_image = align.transform_image(image_bhwc, alignment_transform)
-        guess_loss = align.alignment_loss(alignment_guess_image, target_image, mask_image)
+    for step in range(max_steps):
+        variable_list = [alignment_transform]
 
-    gradient = tape.gradient(guess_loss, alignment_transform)
-    transform_update = gradient * learning_rate_for_lod
-    transform_update = tf.clip_by_value(transform_update, -max_update_for_lod, max_update_for_lod)
-    updated_alignment_transform = alignment_transform - transform_update
-    return updated_alignment_transform, guess_loss
+        with tf.GradientTape() as tape:
+            tape.watch(variable_list)
+            alignment_guess_image = align.transform_image(image_bhwc, alignment_transform)
+            guess_loss = align.alignment_loss(alignment_guess_image, target_image, mask_image)
 
-def align_images(lights, dark_image):
-    # loop below really doesn't need the full average, but we do need the shape and image count, so may as well make a pass
-    average_image_bhwc = lights.read_average_image()
+        gradient = tape.gradient(guess_loss, alignment_transform)
+        transform_update = gradient * learning_rate_for_lod
+        transform_update = tf.clip_by_value(transform_update, -max_update_for_lod, max_update_for_lod)
+        alignment_transform -= transform_update
+        tf.print("loss: ", guess_loss, " after step ", step, " of ", max_steps)
 
-    image_shape = average_image_bhwc.shape
+    return alignment_transform
+
+
+def middle_out(start_index, count):
+    for index in range(start_index + 1, count):
+        yield index, index - 1
+    for index in range(start_index - 1, -1, -1):
+        yield index, index + 1
+
+def compute_alignment_transforms(lights, dark_image = None, target_image_index = None, debug_output_dir = None):
+    image_shape = lights[0].shape
     image_hw = image_shape.as_list()[-3:-1]
 
-    if dark_image is not None:
-        average_image_bhwc = average_image_bhwc - dark_image
-            
-    mask_image = align.generate_mask_image(tf.shape(average_image_bhwc))
+    mask_image = align.generate_mask_image(image_shape)
     util.write_image(mask_image, os.path.join(output_path, 'mask_image.png'))
-
-    util.write_image(average_image_bhwc, os.path.join(output_path, 'initial_average_image.png'), normalize = True)
 
     # todo: fft
     if blur_pixels != 0:
@@ -98,8 +92,6 @@ def align_images(lights, dark_image):
         # have to tile, conv2d crashes rather than broadcasting across the last dimension (output_dim)
         blur_kernel = tf.tile(blur_kernel, multiples = (1, 1, 1, 3))
 
-
-
      # could compute different values for width and height, and even for rotation, but KISS:
     max_dimension = max(image_hw[-2], image_hw[-1])
     min_dimension = min(image_hw[-2], image_hw[-1])
@@ -109,128 +101,114 @@ def align_images(lights, dark_image):
         num_lods += 1
     print(f"num_lods: {num_lods}")
 
-
     # [alignment_param_index: dx, dy, theta]
     identity_alignment_transform = tf.zeros(3)
 
     # [batch, alignment_param_index]
     alignment_transforms = tf.Variable(tf.tile(tf.expand_dims(identity_alignment_transform, axis=0), multiples=(len(lights), 1)))
 
-    losses = tf.Variable(tf.ones(len(lights)))
 
-    #todo: really it should be "align this image with the average so far in this pass", rather than the current embarassingly-parallel approach...
-    #that has some annoying asymmetry to it (maybe helped with exponential moving average?) if there's individual best frames, but makes the problem easier
-    # also todo: instead of this downsampilng business, just do a big fft blur, perhaps with a gaussian or exponential distribution...
+    if target_image_index is None:
+        target_image_index = int(len(lights) / 2)
 
+    
+    target_image = tf.Variable(lights[target_image_index])
+    if dark_image is not None:
+        target_image.assign_sub(dark_image)
+    target_image.assign(with_zero_mean_and_unit_variance(target_image))
 
-    for lod in range(num_lods - 1, skip_lods - 1, -1):
+    # todo: middle-out
+    for image_index, middleward_index in middle_out(target_image_index, len(lights)):
+        image_bhwc = lights[image_index]
+        if dark_image is not None:
+            image_bhwc = image_bhwc - dark_image
 
-        lod_downsample_factor = pow(lod_factor, lod)
-        lod_hw = [int(image_hw[0] / lod_downsample_factor), int(image_hw[1] / lod_downsample_factor)]
-        lod_max_dimension = max(lod_hw[-2], lod_hw[-1])
+        image_bhwc = with_zero_mean_and_unit_variance(image_bhwc)
 
-        learning_rate_for_lod = tf.constant([
-            learning_rate_translation * lod_downsample_factor,
-            learning_rate_translation * lod_downsample_factor,
-            learning_rate_rotation * lod_downsample_factor
-        ])
+        # todo: could do some exponential moving average momentum thingy here
+        if image_index > 0:
+            alignment_transforms[image_index, :].assign(alignment_transforms[middleward_index, :])
 
-        max_update_for_lod = tf.constant([
-            max_update_translation_pixels / lod_max_dimension,
-            max_update_translation_pixels / lod_max_dimension,
-            max_update_rotation_pixels / lod_max_dimension,
-        ])
+        for lod in range(num_lods - 1, skip_lods - 1, -1):
 
-        max_gradient_for_lod = max_update_for_lod / learning_rate_for_lod
-        
-        # recompute the average image, but with the transforms applied
-        # with some cool losses thing?
-        #target_image = read_average_image_with_alignment_transforms(lights, alignment_transforms, losses, image_shape, dark_image)
-        # with just equal weights...
-        #target_image = read_average_image_with_alignment_transforms(lights, alignment_transforms, tf.ones_like(losses), image_shape, dark_image)
-        # eh just get the middle one...
-        target_image = lights[int(len(lights) / 2)]
-        
-        # todo: normalize brightness and contrast
+            lod_downsample_factor = pow(lod_factor, lod)
+            lod_hw = [int(image_hw[0] / lod_downsample_factor), int(image_hw[1] / lod_downsample_factor)]
+            lod_max_dimension = max(lod_hw[-2], lod_hw[-1])
 
-        # now we downsample the target (should be equivalent to doing it in the loop above because linearity)
-        if lod_downsample_factor != 1:
-            target_image = tf.image.resize(target_image, lod_hw, antialias=True)
-            downsampled_mask_image = tf.image.resize(mask_image, lod_hw, antialias=True)
-        else:
-            downsampled_mask_image = mask_image
+            learning_rate_for_lod = tf.constant([
+                learning_rate_translation * lod_downsample_factor,
+                learning_rate_translation * lod_downsample_factor,
+                learning_rate_rotation * lod_downsample_factor
+            ])
 
-        # todo: lerp over pass?
-        # not blurring at the end explodes it
-        if blur_pixels != 0: #and lod_downsample_factor != 1:
-            target_image = tf.nn.conv2d(target_image, blur_kernel, strides = 1, padding = 'SAME')
+            max_update_for_lod = tf.constant([
+                max_update_translation_pixels / lod_max_dimension,
+                max_update_translation_pixels / lod_max_dimension,
+                max_update_rotation_pixels / lod_max_dimension,
+            ])
 
-        util.write_image(target_image, os.path.join(output_path, 'target_image.png'))
+            max_gradient_for_lod = max_update_for_lod / learning_rate_for_lod
+            
+            # todo: memoize downsampled_mask_image across lods
+            # todo: if we don't incrementally_improve_target_image, we could memoize it too
+            if lod_downsample_factor != 1:
+                lod_mask_image = tf.image.resize(mask_image, lod_hw, antialias=True)
+                lod_target_image = tf.image.resize(target_image, lod_hw, antialias=True)
+                lod_image = tf.image.resize(image_bhwc, lod_hw, antialias=True)
+            else:
+                lod_mask_image = mask_image
+                lod_target_image = target_image
+                lod_image = image_bhwc
 
-        target_image = with_zero_mean_and_unit_variance(target_image)
+            if blur_pixels != 0:
+                lod_target_image = tf.nn.conv2d(lod_target_image, blur_kernel, strides = 1, padding = 'SAME')
+                lod_image = tf.nn.conv2d(lod_image, blur_kernel, strides = 1, padding = 'SAME')
 
-        debug = True
-        if debug:
-            equal_weight_image = read_average_image_with_alignment_transforms(lights, alignment_transforms, tf.ones_like(losses), image_shape, dark_image)
-            util.write_image(equal_weight_image, os.path.join(output_path, 'equal_weights_image.png'))
-            del equal_weight_image
-
-        for pass_index in range(passes_per_lod):
-            gc.collect()
-
-            print(f'lod {num_lods - lod} of {num_lods - skip_lods}, pass {pass_index + 1} of {passes_per_lod}')
-
-            # now try to align everything to the new target
-            for image_index, image_bhwc in enumerate(lights):
-                gc.collect()
-
-                if dark_image is not None:
-                    image_bhwc = image_bhwc - dark_image
-
-                if lod_downsample_factor != 1:
-                    image_bhwc = tf.image.resize(image_bhwc, lod_hw, antialias=True)
-
-                image_bhwc = with_zero_mean_and_unit_variance(image_bhwc)
-
-                alignment_transform = tf.Variable(alignment_transforms[image_index, :])
-
-                best_alignment_transform = tf.Variable(alignment_transform)
-
-
-                for steps in range(max_steps):
-                    alignment_transform, guess_loss = align_image_training_step(
-                        image_bhwc,
-                        alignment_transform=alignment_transforms[image_index, :],
-                        target_image=target_image,
-                        mask_image=downsampled_mask_image,
-                        learning_rate_for_lod=learning_rate_for_lod,
-                        max_update_for_lod=max_update_for_lod
-                    )    
-                    
-                    best_alignment_transform.assign(alignment_transform)
-                    losses[image_index].assign(guess_loss[0]) 
+            print(f'Aligning image {image_index} of {len(lights)}, lod {num_lods - lod} of {num_lods - skip_lods}')
+            
+            alignment_transforms[image_index, :].assign(
+                align_image_training_loop(
+                    lod_image,
+                    alignment_transform=alignment_transforms[image_index, :],
+                    target_image=lod_target_image,
+                    mask_image=lod_mask_image,
+                    learning_rate_for_lod=learning_rate_for_lod,
+                    max_update_for_lod=max_update_for_lod,
+                    max_steps = tf.constant(max_steps, dtype=tf.int32)
+                )
+            )
                 
-                alignment_transforms[image_index, :].assign(best_alignment_transform)
 
-                # cener everything
-                alignment_transforms.assign(alignment_transforms - tf.reduce_mean(alignment_transforms, axis = 1, keepdims=True))
+        aligned_image = None
+        if incrementally_improve_target_image or (debug_output_dir is not None):
+            aligned_image = align.transform_image(image_bhwc, alignment_transforms[image_index, :])
 
-            print(f'losses averaged {tf.reduce_mean(losses)}')
+        if incrementally_improve_target_image:
+            new_image_weight = 1.0 / (image_index + 1)
+            target_image.assign(with_zero_mean_and_unit_variance(target_image * (1 - new_image_weight) + aligned_image * new_image_weight))
+
+        if debug_output_dir is not None:            
+            util.write_image(aligned_image, os.path.join(debug_output_dir, f"aligned_{image_index:08d}.png"), normalize = True)
+            if incrementally_improve_target_image:
+                util.write_image(target_image, os.path.join(debug_output_dir, f"target_after_{image_index:08d}.png"), normalize = True)
+
+    alignment_transforms.assign(alignment_transforms - tf.reduce_mean(alignment_transforms, axis = 0, keepdims=True))
     return alignment_transforms
 
 dark_image = None
 if darks is not None:
     dark_image = darks.read_average_image()
 
-alignment_transforms = align_images(lights, dark_image)
+alignment_transforms = compute_alignment_transforms(lights, dark_image=dark_image, debug_output_dir=output_path)
 
 final_average_image_bhwc = read_average_image_with_alignment_transforms(lights, alignment_transforms, lights[0].shape, dark_image)
 util.write_image(final_average_image_bhwc, os.path.join(output_path, 'final_average_image.png'))
 
-for image_index, image_bhwc in lights:
+
+for image_index, image_bhwc in enumerate(lights):
     if dark_image is not None:
         image_bhwc -= dark_image
 
     alignment_transform = alignment_transforms[image_index, :]
     transformed_image_bhwc = align.transform_image(image_bhwc, alignment_transform)
-    util.write_image(transformed_image_bhwc, os.path.join(output_path, "aligned_{:08d}.png".format(image_index)))
+    util.write_image(transformed_image_bhwc, os.path.join(output_path, "final_aligned_{:08d}.png".format(image_index)))
